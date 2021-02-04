bb2426ecb554237e9920386cde573bfd8561aabc
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-04 23:36:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
   if speed < 40
-else speed > 60
+    "unsafe"
+elsif speed > 60
+    "unsafe"
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 ? "not_safe"
+	speed > 60 ? "not_safe"
 end
 	
 
